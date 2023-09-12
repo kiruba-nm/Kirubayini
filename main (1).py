edfee6d2e3bc5aa01672f2902ea95b2e804b8605
kiruba-nm <144709336+kiruba-nm@users.noreply.github.com>
2023-09-12 12:51:31 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,8 @@
+n=int(input("enter year")
+if(n%400==0):                 
+ print("leap year")
+ elif(n%100=0):                           
+  print("not a leap year")                             elif(n%4==0):
+  printf("leap year")        
+  else:                              
+  print("not a leap year")
